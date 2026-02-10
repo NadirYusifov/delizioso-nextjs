27351f3bd32818c26677e7f345d395e7b57d909c
NadirYusifov <nadiryusifov448@gmail.com>
2026-02-10 23:02:11 +0400

    update: card changed style

--- components/foodcard/index.tsx
+++ components/foodcard/index.tsx
@@ -10,13 +10,16 @@ interface props {
 
 export default function FoodCard({ food }: props) {
   return (
-    <div>
-      <div className="card bg-dust-grey/10 w-96 shadow-sm">
+    <div className="group">
+      <div className="card bg-dust-grey/10 group-hover:bg-dark-orange w-[155.34px] h-63.75 lg:w-96 shadow-sm rounded-[70px] px-8 py-5">
         <figure>
           <Image src={""} width={100} height={100} alt="food" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{food.name}</h2>
+        <div className="flex justify-center items-center flex-col">
+          <article className="card-title">
+            <h2 className="text-dark-coffee group-hover:text-white text text-[1.875rem] font-semibold leading-[200%] font-popins">{food.name}</h2>
+          </article>
           <RatingComponents
             name="customize-star"
             max={5}
@@ -25,9 +28,13 @@ export default function FoodCard({ food }: props) {
             icon={<FilledStar />}
             emptyIcon={<EmptyStar />}
           />
-          <p>{food.description}</p>
-          <div className="card-actions justify-end">
-            <Button title="Order now" />
+        </div>
+          <article className="text-center">
+            <p className="text-shadow-deep-walnut-2 group-hover:text-white text-[0.875rem] font-normal font-popins leading-[200%]">{food.description}</p>
+          </article>
+          <div className="card-actions flex justify-between items-center">
+            <p className="text-dark-coffee group-hover:text-white text-[1.563rem] font-semibold leading-[100%] font-popins">${food.price}</p>
+            <Button className="group-hover:bg-white group-hover:text-dark-orange" title="Order now" />
           </div>
         </div>
       </div>
