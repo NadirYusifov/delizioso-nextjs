5f8ac3115fadbc071c43a420ac45edd456034c03
NadirYusifov <nadiryusifov448@gmail.com>
2026-04-01 02:54:12 +0400

    feat: refactor LoginSection component to use custom Input and Button components, enhance layout and styling, and improve user experience with form validation

--- pages/auth/loginauth/index.tsx
+++ pages/auth/loginauth/index.tsx
@@ -5,11 +5,13 @@ import Image from "next/image";
 import { useState } from "react";
 import { useFormik } from "formik";
 import { object, string } from "yup";
+import { Input } from "@/common/input";
+import { Button } from "@/common/button";
+import { Lock, LockOpen } from "lucide-react";
+import LogoDelizosoIcon from "@/public/logoassets/logodeliziosoicon.png"
 import { toast, ToastContainer } from "react-toastify";
 import LoginImage from "@/public/authassets/login1.png";
 import GoogleImage from "@/public/authassets/googlelogo.svg";
-import { Lock, LockOpen } from "lucide-react";
-import { Button } from "@/common/button";
 
 export default function LoginSection() {
   const [showpsw, setShowPsw] = useState(false);
@@ -43,122 +45,35 @@ export default function LoginSection() {
 
   return (
     <section className="h-dvh">
-      <div className="w-full h-screen grid grid-cols-1 lg:grid-cols-2 place-content-center place-items-center">
-        <div className="container">
-          <div>
-            <h3 className="text-[50px] font-bold leading-[100%]">Login</h3>
-            <p className="text-[20px] text-irish-coffee pt-3 pb-10 leading-[200%] font-normal">
-              Don&apos;t have an account?{" "}
-              <Link href="/signup">
-                <span className="text-sky-500">Sign up</span>
-              </Link>
-            </p>
-          </div>
-          <form onSubmit={formik.handleSubmit}>
-            <div className="flex flex-col z-10 space-y-4">
-              <div>
-                <input
-                  className={`w-full bg-infusion/15 p-4 outline-none rounded-lg placeholder:text-grullo ${formik.errors.email && formik.touched.email ? "outline-red-500" : "outline-none"}`}
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="Email address"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-                {formik.errors.email && formik.touched.email ? (
-                  <p className="text-red-500 text-[14px] mt-2">
-                    {formik.errors.email}
-                  </p>
-                ) : null}
+      <div className="relative m-9.75">
+        <picture className="inline">
+          <Image src={LogoDelizosoIcon} width={33} height={33} quality={100} alt="Logo Delzioso Icon"/>
+        </picture>
+      </div>
+      <div className="flex items-center absolute top-0 right-0 left-0">
+        <div className="container px-4 mx-auto">
+          <form>
+            <fieldset className="flex flex-1 flex-col">
+              <legend className="font-raleway font-bold text-[50px] leading-[100%] mt-13">Login</legend>
+              <p className="text-deep-walnut font-popins font-normal text-[20px] leading-[200%]">Don&apos;t have an account? <Link className="text-sky-500 font-medium" href={"/signup"}>Sign up</Link></p>
+              <div className="flex flex-col gap-y-10">
+              <Input className="h-17.5 bg-dust-grey/10 px-3 placeholder:text-grey-olive text-[20px] font-popins leading-[200%] font-normal clas" variant="outline" placeholder="Email address" />
+              <Input className="h-17.5 bg-dust-grey/10 px-3 placeholder:text-grey-olive text-[20px] font-popins leading-[200%] font-normal clas" variant="outline" placeholder="Password" />
               </div>
-              <div className="relative">
-                <input
-                  className={`w-full bg-infusion/15 p-4 border-none outline-none rounded-lg placeholder:text-grullo ${formik.errors.password && formik.touched.password ? "outline-red-500" : "outline-none"}`}
-                  id="password"
-                  name="password"
-                  type={showpsw ? "text" : "password"}
-                  placeholder="Password"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-                <button
-                  type="button"
-                  className="text-[23px] absolute right-4 top-4 text-irish-coffee"
-                  onClick={() => setShowPsw(!showpsw)}
-                >
-                  {showpsw ? (
-                    <span>
-                      <Lock />
-                    </span>
-                  ) : (
-                    <span>
-                      <LockOpen />
-                    </span>
-                  )}
-                </button>
-                {formik.errors.password && formik.touched.password ? (
-                  <p className="text-red-500 text-[14px] mt-2">
-                    {formik.errors.password}
-                  </p>
-                ) : null}
-              </div>
-
-              <div className="text-[16px] flex items-center justify-between text-irish-coffee py-8">
-                <label>
-                  <input
-                    className="accent-dark-orange scale-125"
-                    name="toggle"
-                    type="checkbox"
-                    value="remember me"
-                    onChange={formik.handleChange}
-                  />
-                  <span className="ml-2 text-irish-coffee pl-2">
-                    Remember me
-                  </span>
+              <div className="flex items-center justify-between">
+                <label className="label text-deep-walnut text-[20px] font-normal font-popins leading-[200%]">
+                <input type="checkbox" className="checkbox" />
+                  Remember me
                 </label>
-                <Link href="/">Forget Password?</Link>
+                <Link href={"*"} className="text-deep-walnut text-[20px] font-normal font-popins leading-[200%]">Forget Password?</Link>
               </div>
-
-              <div className="flex flex-col">
-                <Button
-                  onClick={notify}
-                  disabled={!formik.values.toggle}
-                  type="submit"
-                  className={`text-[16px] bg-dark-orange text-white p-4 rounded-lg leading-[100%] font-medium normal-case ${formik.values.toggle ? "cursor-pointer opacity-100" : "cursor-not-allowed opacity-30"}`}
-                >
-                  Log in
-                </Button>
-                <Button
-                  variant="outline"
-                  type="submit"
-                  className="border-solid text-[16px] border-2 border-irish-coffee text-medium-roast rounded-lg p-4 mt-4 leading-[100%] normal-case"
-                >
-                  <Image
-                    className="mr-2"
-                    src={GoogleImage}
-                    width={25}
-                    height={25}
-                    alt="google-logo"
-                  />
-                  Log in with google
-                </Button>
-              </div>
-            </div>
+              <Button className="rounded-[10px] font-medium font-popins text-[20px] leading-[100%]" variant="primary">Log in</Button>
+              <Button className="rounded-[10px] font-medium font-popins text-[20px] leading-[100%]" variant="outline"><Image src={GoogleImage} width={36} height={36} quality={100} alt="Google Logo"/>Log in with google</Button>
+            </fieldset>
           </form>
         </div>
-        <div className="w-full h-screen lg:block">
-          <Image
-            className="w-full"
-            src={LoginImage}
-            width={1000}
-            height={900}
-            quality={100}
-            alt="login-image"
-          />
-        </div>
+        <Image src={LoginImage} width={1350} height={100} quality={100} alt="Login Image" />
       </div>
-      <ToastContainer position="top-center" pauseOnHover draggable stacked />
     </section>
   );
 }
